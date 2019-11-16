8c0f366fb5b2b0baae8f2acd0bd7a0c7e19e7c20
yuyaushiro <yuya.suzuki0829@gmail.com>
2019-11-17 05:34:14 +0900

    Change calc_gradient.py

--- calc_gradient.py
+++ calc_gradient.py
@@ -21,17 +21,14 @@ if __name__ == "__main__":
         d = line.split()
         value_data[int(d[0]), int(d[1])] = float(d[2])
 
-    gradients = np.zeros(np.r_[state_num, 4])
-    for i, value in enumerate(values):
-        index = value[0:2]
-        index_up = intoMap(index + np.array([1, 1]), cell_nums)
-        index_low = intoMap(index + np.array([-1, -1]), cell_nums)
-
-        x_grad = value_data[int(index_up[0]), int(index[1])] - value_data[int(index_low[0]), int(index[1])]
-        y_grad = value_data[int(index[0]), int(index_up[1])] - value_data[int(index[0]), int(index_low[1])]
-
-        gradients[i] = np.array([value[0], value[1], x_grad, y_grad])
-
     with open('gradient/' + file_name + '.grad', 'w') as f:
-        for gradient in gradients:
+        for i, value in enumerate(values):
+            index = value[0:2]
+            index_up = intoMap(index + np.array([1, 1]), cell_nums)
+            index_low = intoMap(index + np.array([-1, -1]), cell_nums)
+
+            x_grad = value_data[int(index_up[0]), int(index[1])] - value_data[int(index_low[0]), int(index[1])]
+            y_grad = value_data[int(index[0]), int(index_up[1])] - value_data[int(index[0]), int(index_low[1])]
+
+            gradient = np.array([value[0], value[1], x_grad, y_grad])
             f.write("{} {} {} {}\n".format(int(gradient[0]), int(gradient[1]), gradient[2], gradient[3]))
